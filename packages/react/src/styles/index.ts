da2330af230ef382607c31b61f3f61fe7865eb3e
Vander Lima <vnd.vander@gmail.com>
2023-04-10 00:24:21 -0300

    config Stitches and Storybook

---
+++ packages/react/src/styles/index.ts
@@ -0,0 +1,38 @@
+import {
+  colors,
+  fontSizes,
+  fontWeights,
+  fonts,
+  lineHeights,
+  radis,
+  space,
+} from '@ignite-ui/tokens'
+
+import { createStitches, defaultThemeMap } from '@stitches/react'
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  themeMap: {
+    ...defaultThemeMap,
+    height: 'space',
+    with: 'space',
+  },
+
+  theme: {
+    colors,
+    fontSizes,
+    fontWeights,
+    fonts,
+    lineHeights,
+    radii: radis,
+    space,
+  },
+})
