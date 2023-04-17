86367068963031fcbe66c39499608485b35d8d4e
Vander Lima <vnd.vander@gmail.com>
2023-04-17 00:20:23 -0300

    docs: Control on Components [2]

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -9,6 +9,13 @@ export default {
     src: 'https://github.com/vanderL.png',
     alt: 'Vander Lima de Andrade',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -6,8 +6,31 @@ export default {
   title: 'Typograpy/Text',
   component: Text,
   args: {
+    size: 'md',
     children:
       'Lorem ipsum dolor sit, amet consectetur adipisicing elit. In, sapiente earum odit sunt perspiciatis, quasi officiis nulla mollitia harum itaque, quaerat nobis at necessitatibus voluptates voluptas voluptatum quibusdam nemo! Eaque.',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
   },
 } as Meta<TextProps>
 
