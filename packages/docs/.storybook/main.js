507bcc10a1b1819b4a46ce4a30273b8a1a9b6763
Vander Lima <vnd.vander@gmail.com>
2023-04-18 01:18:11 -0300

    fix: upgrade

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,20 +1,20 @@
-/** @type { import('@storybook/react-vite').StorybookConfig } */
-const config = {
-  stories: ["../src/pages/**/*.mdx", "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
+module.exports = {
+  "stories": [
+    "../src/pages/**/*.stories.mdx",
+    "../src/stories/**/*.stories.tsx"
+  ],
+  "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/addon-a11y"
+    "@storybook/addon-a11y",
   ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-vite"
   },
-  docs: {
-    autodocs: "tag",
-    defaultName: 'Documentation',
-
+  "features": {
+    "storyStoreV7": true
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
@@ -23,5 +23,4 @@ const config = {
 
     return config
   }
-};
-export default config;
+}
